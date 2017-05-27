0d075717f253297f7c2cd5d1d5c17f6684efeab7
sah4ez <sah4ez32@gmail.com>
2017-05-27 08:20:47 +0300

    Create RU localization. Translate account- and change-management.

---
+++ app/js/localization/nls/ru/product-structure.js
@@ -0,0 +1,13 @@
+/*global define*/
+define({
+    root: {
+        CASCADE: 'Cascade',
+        CASCADE_RESULT: 'Result',
+        DONE: 'Done',
+        SELECTED: 'Selected',
+        PRODUCT_VIEW: 'Product view',
+        STRUCTURE: 'Product structure'
+    },
+    fr: true,
+    es: true
+});
