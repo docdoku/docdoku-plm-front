ea1cd9a43431e5d0bcc8b247da1d72c6f8ece708
sah4ez <sah4ez32@gmail.com>
2017-06-01 09:23:16 +0300

    Formatting and translate document-management

--- app/js/localization/nls/ru/document-management.js
+++ app/js/localization/nls/ru/document-management.js
@@ -1,11 +1,7 @@
 /*global define*/
 define({
-    root: {
-        CONFIRM_DELETE_DOCUMENT_COLLECTIONS:'Do you want to delete selected document collections?',
-        DOCUMENT_NOT_RELEASED:'This document is not released and cannot be added to the released collection',
-        DOCUMENT_ALREADY_IN_LIST:'This document is already present in the collection',
-        DOCUMENT_BASELINE_IS_EMPTY:'You cannot create an empty document collection'
-    },
-    'fr': true,
-    'es': true
+    CONFIRM_DELETE_DOCUMENT_COLLECTIONS: 'Вы хотите удалить выбранные колекции документов?',
+    DOCUMENT_NOT_RELEASED: 'Этот документ не опубликован и не может быть добавить в колекции опубликованных',
+    DOCUMENT_ALREADY_IN_LIST: 'Этот документ уже есть в коллекции',
+    DOCUMENT_BASELINE_IS_EMPTY: 'Вы не можете создать пустую коллекцию документов'
 });
