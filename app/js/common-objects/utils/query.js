5a8206b016d33cb9eef6e6f357fff5f5690d9600
Hunsu <rabah.meradi@docdoku.com>
2018-07-17 16:32:27 +0200

    Escape ; and : characters in search query
    Ref #50

--- app/js/common-objects/utils/query.js
+++ app/js/common-objects/utils/query.js
@@ -12,13 +12,14 @@ define([], function () {
             });
             if (attributes.length) {
                 queryString += '&attributes=';
+                var that = this;
                 attributes.each(function (attribute) {
                     var type = attribute.get('type') || attribute.get('attributeType');
                     var name = attribute.get('name');
                     var value = attribute.get('value') || '';
                     value = type === 'BOOLEAN' ? (value ? 'true' : 'false') : value;
                     value = type === 'LOV' ? attribute.get('items')[value].name : value;
-                    queryString += type + ':' + name + ':' + value + ';';
+                    queryString += type + ':' + name + ':' + that.sanitize(value) + ';';
                 });
                 // remove last '+'
                 queryString = queryString.substr(0, queryString.length - 1);
@@ -27,6 +28,12 @@ define([], function () {
             queryString += '&from=0&size=10000';
             return queryString;
         },
+
+        sanitize : function(value) {
+            value = value.replace('\\', '\\\\');
+            value = value.replace(':', '\\:');
+            return value.replace(';', '\\;');
+        }
     };
 
     return Query;
