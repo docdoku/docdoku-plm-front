44c061b062e388f29b60dc593261339088d5cf1b
Morgan Guimard <morgan.guimard@docdoku.com>
2020-08-10 08:49:56 +0200

    use version from package.json, docker build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+IMAGE_NAME=docdoku/docdoku-web-front
+VERSION=$(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
+
+
+npm run build && \
+docker build -f docker/Dockerfile -t $IMAGE_NAME:latest . && \
+docker tag $IMAGE_NAME:latest $IMAGE_NAME:$VERSION
+
+
