4a82d60b464b86e45e0f91a8705dd2db68fa86ca
Morgan Guimard <morgan.guimard@docdoku.com>
2020-08-12 08:10:33 +0200

    docker image review - use volume for properties file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env sh
 
-echo -e "{
-    \"server\": {
-        \"ssl\": $SERVER_SSL,
-        \"domain\": \"$SERVER_DOMAIN\",
-        \"port\": $SERVER_PORT,
-        \"contextPath\": \"$SERVER_CONTEXT_PATH\",
-        \"wsDomain\": \"$SERVER_WS_DOMAIN\"
-    },
-    \"contextPath\": \"$CONTEXT_PATH\",
-    \"preferLoginWith\": $PREFER_LOGIN_WITH
-}" > /usr/share/nginx/html/webapp.properties.json
-
-cat  /usr/share/nginx/html/webapp.properties.json
-
 /usr/sbin/nginx -g "daemon off;"
 
