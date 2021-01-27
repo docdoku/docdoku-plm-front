d2edd2a0b8e04a2a0145364b611362ec3c0a6d65
Morgan Guimard <morgan.guimard@docdoku.com>
2021-01-27 12:17:20 +0100

    Review build script

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env sh
 
-IMAGE_NAME=docdoku/docdoku-plm-front
+set -e
+
 VERSION=$(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
 
-
-npm run build && \
-docker build -f docker/Dockerfile -t $IMAGE_NAME:latest . && \
-docker tag $IMAGE_NAME:latest $IMAGE_NAME:$VERSION
-
-
+npm run build
+docker build -f docker/Dockerfile -t docdoku/docdoku-plm-front:$VERSION .
